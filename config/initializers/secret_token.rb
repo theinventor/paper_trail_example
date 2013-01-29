d25568f5a5fe37d09f2920429fc9d481eadf9110
Troy Anderson <Troy.Anderson@citygridmedia.com>
2013-01-29 10:52:44 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperTrailExample::Application.config.secret_token = '0141da3edf337667b6a75235ea1f400cb76a031397a17b5bbe21ae176e0a3a1571392bc67391ccd7edc4ea55c004754b062a0abb5c9f60452483e8d76d094477'
